d07fc596f7de8150168193c5b30647d8fc274927
Marcel Wolf <marcel.wolf@me.com>
2015-11-21 14:24:55 +0100

    flake8

--- Hugofy.py
+++ Hugofy.py
@@ -1,87 +1,115 @@
-import sublime, sublime_plugin,subprocess,os
+import sublime
+import sublime_plugin
+import subprocess
+import os
+
 
 def setvars():
-	global settings,path,sitename
-	settings=sublime.load_settings("hugofy.sublime-settings")
-	path=settings.get("Directory")
-	sitename=settings.get("Sitename")
-	if not os.path.exists(os.path.join(path,sitename)):
-		 os.makedirs(os.path.join(path,sitename))
-	os.chdir(os.path.join(path,sitename))
+    global settings, path, sitename
+    settings = sublime.load_settings("hugofy.sublime-settings")
+    path = settings.get("Directory")
+    sitename = settings.get("Sitename")
+    if not os.path.exists(os.path.join(path, sitename)):
+        os.makedirs(os.path.join(path, sitename))
+    os.chdir(os.path.join(path, sitename))
+
 
 class HugonewsiteCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		setvars()
-		process="hugo new site "+os.path.join(path,sitename)
-		subprocess.Popen(process)
+
+    def run(self, edit):
+        setvars()
+        process = "hugo new site "+os.path.join(path, sitename)
+        subprocess.Popen(process)
+
 
 class HugonewcontentCommand(sublime_plugin.TextCommand):
-	def on_done(self,pagename):
-		if not pagename:
-			sublime.error_message("No filename provided")
-		process="hugo new "+pagename
-		subprocess.Popen(process)
-		sublime.active_window().open_file(os.path.join(path,sitename,"content",pagename))
-	def on_change(self,filename):
-		pass
-	def on_cancel(self):
-		sublime.error_message("No filename provided")
-	def run(self,edit):
-		setvars()
-		sublime.active_window().show_input_panel("Enter file name", "", self.on_done, self.on_change, self.on_cancel)
+
+    def on_done(self, pagename):
+        if not pagename:
+            sublime.error_message("No filename provided")
+        process = "hugo new "+pagename
+        subprocess.Popen(process)
+        sublime.active_window().open_file(
+            os.path.join(path, sitename, "content", pagename))
+
+    def on_change(self, filename):
+        pass
+
+    def on_cancel(self):
+        sublime.error_message("No filename provided")
+
+    def run(self, edit):
+        setvars()
+        sublime.active_window().show_input_panel(
+            "Enter file name", "", self.on_done, self.on_change, self.on_cancel)
+
 
 class HugoversionCommand(sublime_plugin.TextCommand):
-	def run(self,edit):
-		try:
-			out=subprocess.check_output(["hugo", "version"],stderr=subprocess.STDOUT,universal_newlines=True)
-			sublime.message_dialog(out)
-		except:
-			sublime.error_message("Hugo not installed or path not set")
+
+    def run(self, edit):
+        try:
+            out = subprocess.check_output(
+                ["hugo", "version"], stderr=subprocess.STDOUT, universal_newlines=True)
+            sublime.message_dialog(out)
+        except:
+            sublime.error_message("Hugo not installed or path not set")
+
 
 class HugoserverCommand(sublime_plugin.TextCommand):
-	def run(self,edit):
-		setvars()
-		server=settings.get("Server")
-		theme=settings.get("DefaultTheme")
-		try:
-			out=subprocess.Popen("hugo server --theme="+theme+"--buildDrafts --watch --port="+server["PORT"],stderr=subprocess.STDOUT,universal_newlines=True)
-			sublime.status_message(out)
-		except:
-			sublime.error_message("Error starting server")
+
+    def run(self, edit):
+        setvars()
+        server = settings.get("Server")
+        theme = settings.get("DefaultTheme")
+        try:
+            out = subprocess.Popen("hugo server --theme="+theme+"--buildDrafts --watch --port="+server[
+                                   "PORT"], stderr=subprocess.STDOUT, universal_newlines=True)
+            sublime.status_message(out)
+        except:
+            sublime.error_message("Error starting server")
 
 
 class HugobuildCommand(sublime_plugin.TextCommand):
-	def run(self,edit):
-		try:
-			out=subprocess.Popen("hugo --buildDrafts",stdout=subprocess.PIPE)
-			#print(out.communicate()[0].decode('utf-8'))
-			sublime.message_dialog(out.communicate()[0].decode('utf-8'))
-		except:
-			sublime.error_message("Hugo not installed")
+
+    def run(self, edit):
+        try:
+            out = subprocess.Popen(
+                "hugo --buildDrafts", stdout=subprocess.PIPE)
+            # print(out.communicate()[0].decode('utf-8'))
+            sublime.message_dialog(out.communicate()[0].decode('utf-8'))
+        except:
+            sublime.error_message("Hugo not installed")
+
 
 class HugogetthemesCommand(sublime_plugin.TextCommand):
-	"""download themes for hugo"""
-	def run(self,edit):
-		setvars()
-		try:
-			out=subprocess.Popen("git clone --recursive https://github.com/spf13/hugoThemes.git "+os.path.join(path,sitename,"themes"),stderr=subprocess.STDOUT,universal_newlines=True)
-		except:
-			sublime.error_message("git not installed or path not set")
+
+    """download themes for hugo"""
+
+    def run(self, edit):
+        setvars()
+        try:
+            out = subprocess.Popen("git clone --recursive https://github.com/spf13/hugoThemes.git "+os.path.join(
+                path, sitename, "themes"), stderr=subprocess.STDOUT, universal_newlines=True)
+        except:
+            sublime.error_message("git not installed or path not set")
+
 
 class HugosetthemeCommand(sublime_plugin.TextCommand):
-	def on_done(self,themename):
-		if not themename:
-			sublime.error_message("No theme name provided")
-		else:
-			settings.set("DefaultTheme",themename)
-			sublime.save_settings("hugofy.sublime-settings")
 
-	def on_change(self,themename):
-		pass
-	def on_cancel(self):
-		pass
+    def on_done(self, themename):
+        if not themename:
+            sublime.error_message("No theme name provided")
+        else:
+            settings.set("DefaultTheme", themename)
+            sublime.save_settings("hugofy.sublime-settings")
 
-	def run(self,edit):
-		setvars()
-		sublime.active_window().show_input_panel("Enter theme name", "", self.on_done, self.on_change, self.on_cancel)
-		
+    def on_change(self, themename):
+        pass
+
+    def on_cancel(self):
+        pass
+
+    def run(self, edit):
+        setvars()
+        sublime.active_window().show_input_panel(
+            "Enter theme name", "", self.on_done, self.on_change, self.on_cancel)
